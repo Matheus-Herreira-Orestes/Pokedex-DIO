f4ca4b63edf3d2b21faa32360a0b849a681badc7
Matheus <mherreirao@gmail.com>
2023-10-04 15:48:23 -0300

    Segunda Pagina Funcionando/ Com protocolo HTTP

--- assets/js/pokeAbout.js
+++ assets/js/pokeAbout.js
@@ -11,14 +11,14 @@ function pokeCreate (pokemon){
         <div class="upper">
             <div class="header">
                 <h1>${pokemon.name}</h1>
-                <h1>#${pokemon.number}</h1>
+                <h1>#${pokemon.id}</h1>
             </div>
             <ol class="types">
 
 
             </ol>
 
-            <img src="${pokemon.photo}" alt="${pokemon.name}">
+            <img src="${pokemon.sprites.other.dream_world.front_default}" alt="${pokemon.name}">
         </div>
         
 
@@ -27,12 +27,12 @@ function pokeCreate (pokemon){
                 <h2>About</h2>
 
                 <ul>
-                    <li>Hp:${pokemon.hp}</li>
-                    <li>Attack:${pokemon.attack}</li>
-                    <li>Defense:${pokemon.defense}</li>
-                    <li>Special Attack:${pokemon.special_attack}</li>
-                    <li>Special Defense:${pokemon.special_defense} </li>
-                    <li>Speed:${pokemon.speed} </li>
+                    <li>Hp:${pokemon.stats[0].base_stat}</li>
+                    <li>Attack:${pokemon.stats[1].base_stat}</li>
+                    <li>Defense:${pokemon.stats[2].base_stat}</li>
+                    <li>Special Attack:${pokemon.stats[3].base_stat}</li>
+                    <li>Special Defense:${pokemon.stats[4].base_stat} </li>
+                    <li>Speed:${pokemon.stats[5].base_stat} </li>
                     <li>Weight:${pokemon.weight} </li>
                     <li>Height:${pokemon.height} </li>
                 </ul>
